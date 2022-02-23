cd5e3b2995eb7129f3b7f432e0cbf4a5f6c2359c
Rahul Kherajani <rh346685@dal.ca>
2022-02-23 19:32:17 -0400

    Home Activity

---
+++ app/src/main/java/com/example/mathwiz/ui/categories/CategoriesViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.mathwiz.ui.categories
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CategoriesViewModel : ViewModel() {
+
+        private val _text = MutableLiveData<String>().apply {
+        value = "Card"
+    }
+    val text: LiveData<String> = _text
+}

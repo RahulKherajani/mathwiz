209ab90f9ea6a0a1619f42097e4076cb73153cf2
Kalim <kalim.nanji@gmail.com>
2022-03-24 17:43:23 -0300

    Signup creates user entry in Firestore

--- app/src/main/java/com/example/mathwiz/ui/login/SignupFragment.kt
+++ app/src/main/java/com/example/mathwiz/ui/login/SignupFragment.kt
@@ -3,19 +3,18 @@ package com.example.mathwiz.ui.login
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
-import com.example.mathwiz.HomeActivity
-import com.example.mathwiz.MathWiz
-import com.example.mathwiz.R
-import com.example.mathwiz.auth
+import com.example.mathwiz.*
 import com.example.mathwiz.databinding.FragmentSignupBinding
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.DocumentReference
 
 class SignupFragment : Fragment() {
 
@@ -48,6 +47,9 @@ class SignupFragment : Fragment() {
             else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                 Snackbar.make(view, "Invalid email address. Please re-enter.", BaseTransientBottomBar.LENGTH_SHORT).show()
             }
+            else if(password.length < 6){
+                Snackbar.make(view, "Password must be greater than 6 characters.", BaseTransientBottomBar.LENGTH_SHORT).show()
+            }
             //If the passwords do not match
             else if(!password.equals(confirmPassword)){
                 Snackbar.make(view, "Passwords do not match. Please re-enter.", BaseTransientBottomBar.LENGTH_SHORT).show()
@@ -58,6 +60,20 @@ class SignupFragment : Fragment() {
                     if(task.isSuccessful){
                         //Save the email locally
                         MathWiz.userData?.email = email
+
+                        //Create user entry in database
+                        var userID = auth.currentUser?.uid
+                        if (userID != null) {
+                            var documentReference : DocumentReference
+                            documentReference = fstore.collection("users").document(userID)
+                            var user : HashMap<String, String> = HashMap()
+                            user.put("email", email)
+                            documentReference.set(user).addOnCompleteListener { task2 ->
+                                if (task2.isSuccessful) {
+                                    Log.e("SignupFragment","User entry created")
+                                }
+                            }
+                        }
 
                         Snackbar.make(view, "Sign up successful!", BaseTransientBottomBar.LENGTH_SHORT).show()
                         if(MathWiz.userData?.name == ""){
@@ -72,8 +88,6 @@ class SignupFragment : Fragment() {
                 }.addOnFailureListener { exception ->
                     Snackbar.make(view,exception.localizedMessage,BaseTransientBottomBar.LENGTH_SHORT).show()
                 }
-
-
             }
         }
 

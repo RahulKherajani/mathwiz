9f68b6b40e564c30e500479bfe3cd95e986c4970
Kalim <kalim.nanji@gmail.com>
2022-03-24 16:02:22 -0300

    Added ChangePassword functionality

--- app/src/main/java/com/example/mathwiz/ui/login/ChangePasswordFragment.kt
+++ app/src/main/java/com/example/mathwiz/ui/login/ChangePasswordFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.mathwiz.R
+import com.example.mathwiz.auth
 import com.example.mathwiz.databinding.FragmentChangePasswordBinding
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
@@ -29,35 +30,28 @@ class ChangePasswordFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //TODO send security code
-        val securityCode = "1111"
-
         binding.savePasswordButton.setOnClickListener {
 
-            val securityCodeInput = binding.etSecurityCode.text.toString()
-            val newPassword = binding.etNewPassword.text.toString()
-            val confirmPassword = binding.etConfirmPassword.text.toString()
+            val email = binding.etChangePasswordEmail.text.toString()
 
             //If any of the fields are empty
-            if(TextUtils.isEmpty(securityCodeInput) || TextUtils.isEmpty(confirmPassword) || TextUtils.isEmpty(newPassword)){
+            if(TextUtils.isEmpty(email)){
                 Snackbar.make(view, "Please fill all fields before continuing.", BaseTransientBottomBar.LENGTH_SHORT).show()
             }
-            //If the passwords do not match
-            else if(!newPassword.equals(confirmPassword)){
-                Snackbar.make(view, "Passwords do not match. Please re-enter your password.", BaseTransientBottomBar.LENGTH_SHORT).show()
-            }
-            //If the security code does not match
-            else if (!securityCode.equals(securityCodeInput)){
-                Snackbar.make(view, "Security code does not match. Please re-enter the code.", BaseTransientBottomBar.LENGTH_SHORT).show()
+            else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Snackbar.make(view, "Invalid email address. Please re-enter.", BaseTransientBottomBar.LENGTH_SHORT).show()
             }
             //Otherwise, success
             else {
 
-                //TODO - change password
-
-                findNavController().navigate(R.id.action_ChangePasswordFragment_to_LoginFragment)
-                //Password change success message
-                Snackbar.make(view, "Password change successful!", BaseTransientBottomBar.LENGTH_SHORT).show()
+                //Send password reset email
+                auth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        Snackbar.make(view, "Password reset email sent.", BaseTransientBottomBar.LENGTH_SHORT).show()
+                    }
+                }.addOnFailureListener { exception ->
+                    Snackbar.make(view, exception.localizedMessage, BaseTransientBottomBar.LENGTH_SHORT).show()
+                }
             }
         }
 

77d27a54159d8efe272cea9dda8ff469f2e19827
Kalim <kalim.nanji@gmail.com>
2022-03-25 16:42:48 -0300

    Added Reminders layout + functionality

--- app/src/main/java/com/example/mathwiz/ui/reminders/EditReminderFragment.kt
+++ app/src/main/java/com/example/mathwiz/ui/reminders/EditReminderFragment.kt
@@ -1,18 +1,19 @@
 package com.example.mathwiz.ui.reminders
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.Toast
+import android.widget.Button
+import android.widget.TimePicker
+import androidx.annotation.RequiresApi
 import androidx.navigation.fragment.findNavController
 import com.example.mathwiz.R
+import com.example.mathwiz.ui.reminders.ReminderModel
 import com.example.mathwiz.ui.reminders.MyPersistence
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,21 +22,22 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ReminderFragments.newInstance] factory method to
+ * Use the [EditReminderFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class RemindersFragment : Fragment(), AdapterView.OnItemClickListener {
+class EditReminderFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var position: Int? = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+            position = it.getInt("position")
         }
-
     }
 
     override fun onCreateView(
@@ -43,25 +45,24 @@ class RemindersFragment : Fragment(), AdapterView.OnItemClickListener {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reminders, container, false)
+        return inflater.inflate(R.layout.fragment_edit_reminder, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val notesListViewUI: ListView = view.findViewById(R.id.list_item)
-        notesListViewUI.adapter = ReminderAdapter(
-            view.context,
-            MyPersistence.reminders
-        )
-        notesListViewUI.setOnItemClickListener(this)
-
-        view.findViewById<FloatingActionButton>(R.id.floating_btn_reminder).setOnClickListener {
-            findNavController().navigate(R.id.action_navigation_reminders_to_addReminderFragment)
+        view.findViewById<Button>(R.id.button5).setOnClickListener {
+            var o = MyPersistence.reminders.get(this.position!!)
+            var timePicker = view.findViewById<TimePicker>(R.id.time_picker)
+            o.hour = timePicker.hour
+            o.min = timePicker.minute
+            findNavController().popBackStack()
         }
-
+        view.findViewById<Button>(R.id.button8).setOnClickListener {
+            var o = MyPersistence.reminders.get(this.position!!)
+            MyPersistence.reminders.remove(o)
+            findNavController().popBackStack()
+        }
     }
-
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -69,22 +70,16 @@ class RemindersFragment : Fragment(), AdapterView.OnItemClickListener {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ReminderFragment.
+         * @return A new instance of fragment EditReminderFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            RemindersFragment().apply {
+            EditReminderFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
-
-    override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-        val b = Bundle()
-        b.putInt("position", p2)
-        findNavController().navigate(R.id.action_navigation_reminders_to_editReminderFragment, b)
     }
 }
